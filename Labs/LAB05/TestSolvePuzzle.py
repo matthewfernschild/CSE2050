6e0e1bf53e5020b4f68b0996aa13b08e9a0572a2
Matthew Fernschild <matthew.fernschild@gmail.com>
2026-02-27 18:49:35 -0500

    LAB 05 - Solving a puzzle with Recursion

--- Labs/LAB05/TestSolvePuzzle.py
+++ Labs/LAB05/TestSolvePuzzle.py
@@ -4,14 +4,18 @@ import unittest
 class TestSolvePuzzle(unittest.TestCase):
         def testClockwise(self):
                 """Tests a board solveable using only CW moves"""
+                self.assertTrue(puzzle([2,0,0]))
 
         def testCounterClockwise(self):
                 """Tests a board solveable using only CCW moves"""
-
+                self.assertTrue(puzzle([1,0,0]))
         def testMixed(self):
                 """Tests a board solveable using only a combination of CW and CCW moves"""
+                self.assertTrue(puzzle([2,0,0,1,0]))
         
         def testUnsolveable(self):
                 """Tests an unsolveable board"""
+                self.assertFalse(puzzle([0,1]))
+                self.assertFalse(puzzle([5,0,2,0,0,0,1]))
 
 unittest.main()
--- Labs/LAB05/solve_puzzle.py
+++ Labs/LAB05/solve_puzzle.py
@@ -1,23 +1,26 @@
 def solve_puzzle(L): # Make sure to add input parameters here
     """Returns True(False) if a given board is (is not) solveable"""
-    idx = 0
-    goal = len(L) - 1
-
-    visiting = set()
-
-    if idx == -1 :
-        return True
-    else:
-        idx += L[idx]
-        
-    return _solve_puzzle(L,idx=0,visited=set())
-# if current index == lst[-1], then we solved the puzzle
-
+    visited = set()
+    return _solve_puzzle(L,0,visited)
 
 def _solve_puzzle(L,idx,visited):
-    """"""
-    if idx == -1:
+    ''' the internal puzzle solver'''
+    goal = len(L) - 1
+
+    if idx == goal: # winner winner
         return True
+    
+    if idx in visited:
+        return False
+    visited.add(idx)
 
 
-solve_puzzle([3, 6, 4, 1, 3, 4, 2, 0])
+    tiles = L[idx] # of tiles to move
+
+    if tiles == 0:
+        return False
+    
+    clockwise = (idx + tiles) % len(L)
+    counterclockwise = (idx - tiles) % len(L)
+
+    return _solve_puzzle(L,clockwise,visited) or _solve_puzzle(L,counterclockwise,visited)
