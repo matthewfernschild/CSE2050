18f357f8ae577ab8623b15a4b3466d792549f40a
Matthew Fernschild <matthew.fernschild@gmail.com>
2026-04-27 00:12:24 -0400

    hw10: worked more on creating maxheap

--- Homework/HW10/TaskManager.py
+++ Homework/HW10/TaskManager.py
@@ -17,9 +17,9 @@ class Entry:
         return self.priority > other.priority
     
     def __eq__(self, other):
-        """Checks if this entry is equal to another entry based on priority.
+        """Checks if this entry is eq   ual to another entry based on priority.
         Returns:bool: True if the priorities are equal, False otherwise."""
-        return self.item == other.item and self.priority == other.priority
+        return self.process_id == other.process_id and self.priority == other.priority
     
 
 class MaxHeap:
@@ -33,11 +33,24 @@ class MaxHeap:
     def put(self, entry):
         """Inserts an entry into the max heap."""
         self._heap.append(entry)
+    
+        i = len(self._heap) - 1
+
+        while i > 1 and self._heap[i // 2] < self._heap[i]:
+            temp = self._heap[i]
+            self._heap[i] = self._heap[i//2]
+            self._heap[i//2] = temp
+            i = i // 2
 
     def remove_max(self):
         """Removes and returns the entry with the maximum priority from the max heap.
         Returns:The process ID that was removed from the queue. raise IndexError if the heap is empty"""
-
+        if len(self._heap) == 0:
+            raise IndexError
+        returner = self._heap[0].process_id
+        self._downheap(0)
+        return returner
+    
     def change_priority(self, process_id, new_priority):
         """Changes the priority of a process in the max heap.
         Returns:bool: True if the priority change was successful, False otherwise."""
@@ -45,9 +58,22 @@ class MaxHeap:
 
     def _upheap(self, index):
         """Performs up-heap operation to maintain heap property after insertion."""
+        i = index
+
+        while i > len(self._heap)-1:
+            temp = self._heap[i]
+            self._heap[i] = self._heap[i//2]
+            self._heap[i // 2] = temp
+            i = i // 2 
  
     def _downheap(self, index):
         """Performs down-heap operation to maintain heap property after removal."""
+        i = index
+        while i < len(self._heap)-1:
+            temp = self._heap[i]
+            self._heap[i] = self._heap[i*2]
+            self._heap[i*2] = temp
+            i = i * 2   
 
     def __len__(self):
         """len is number of items in PQ"""    
